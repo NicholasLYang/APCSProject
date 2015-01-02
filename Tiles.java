545f47e5c376cf76d1892218635e37e511d213c3
Sarah Chen <sarah.jia.chen@gmail.com>
2015-01-02 15:35:49 -0500

    added Tiles

---
+++ Tiles.java
@@ -0,0 +1,17 @@
+
+public class Tiles {
+    private static int points;
+
+    public Tiles() {
+	points = 0;
+    }
+
+    public int getPoints() {
+	return points;
+    }
+    
+    public void setPoints(int i) {
+	points = points + i;
+    }
+
+}
