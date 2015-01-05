f6453d19c7f9a81a13c7f972474e802f75dce6b2
Sarah Chen <sarah.chen@cslab1>
2015-01-05 08:18:15 -0500

    stuff

---
+++ Board.java
@@ -0,0 +1,16 @@
+
+public class Board {
+    private Tiles[][] board;
+
+    public Board() {
+	board = new Tiles[15][15];
+	for(int r=0; r<board.length;r++) {
+	    for(int c=0; c<board.length; c++) {
+	    //set something
+	    }
+	}
+    }
+
+    
+
+}
--- Tiles.java
+++ Tiles.java
@@ -1,9 +1,21 @@
 
 public class Tiles {
-    private static int points;
+    private int points;
+    private boolean doubleWord, tripleWord, doubleLetter, tripleLetter = false;
 
     public Tiles() {
 	points = 0;
+    }
+    public Tiles(int i) {
+	points = 0;
+	if(i==0) { doubleLetter = true;	}
+	else if(i==1) {tripleLetter = true;}
+	if(i==2) { doubleWord = true;}
+	else if(i==3) {tripleWord = true;}
+	else {
+	    doubleWord = false;
+	    tripleWord = false;
+	}
     }
 
     public int getPoints() {
