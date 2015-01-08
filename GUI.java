c59adf5c9782616e65c0087c3bc3afd9b47998a7
Nicholas Yang <nicholas@MacBook-Pro-de-Nicholas.local>
2015-01-08 11:58:54 -0500

    tried to implement a loop to draw the board. Ran into some errors w/LetterVal, can you explain code?

--- GUI.java
+++ GUI.java
@@ -4,18 +4,43 @@ import java.awt.*;
 public class GUI extends JFrame {
     private Container pane;
     private JTextField name1,name2,name3,name4;
-
+    private JPanel canvas;
+    
     public GUI() {
 	setTitle("Scrabble");
 	setSize(700,700);
 	setLocation(100,100);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+	Board b = new Board();
 	pane = getContentPane();
-	name1, name2, name3, name4 = new TextField();
+	canvas = new Canvas();
+	canvas.setPreferredSize(new Dimension(2250,2250));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+	canvas.printBoard(b);
+	pane.add(canvas);
+	TextField name1, name2, name3, name4 = new TextField();
 	
 	
     }
+
+    private class Canvas extends JPanel
+    {
+	public void printBoard(Board b, Graphics g)
+	{
+	    Tile[][] board = b.getBoard();
+	    for (int i = 0; i < board.length; i++)
+		{
+		    for (int i = 0; i < board[1].length; i++)
+			{
+			    canvas.paintCompnent(g);
+			    g.setColor(Color.gray);
+			    g.fillRect(0,0,30,30);
+			}
+		}
+	}
+    }
+			
 
     public static void main(String[] args) {
 	GUI x = new GUI();
