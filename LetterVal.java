3e8edfe7621f1014f5421112b8b5200ffbec1f3f
Sarah Chen <sarah.chen@cslab1>
2015-01-09 08:28:31 -0500

    fixed it!

--- LetterVal.java
+++ LetterVal.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class LetterVal {
-    private Map<char,int> LValues = new HashMap<char,int>();
+    private Map<Character, Integer> LValues = new HashMap<Character, Integer>();
     private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public LetterVal() {
 	for(int i=0; i<alphabet.length(); i++) {
-	    LValues.put(alphabet[i], 1);
+	    LValues.put(alphabet.charAt(i), 1);
 	}
     }
 
