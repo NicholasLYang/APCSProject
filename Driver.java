60a95aa0cb17a979e266008f33a0b60e4b54c60a
Nicholas Yang <nick@nicholasyang.com>
2015-01-15 09:21:01 -0500

    Merge branch 'master' of https://github.com/NicholasLYang/APCSProject

---
+++ Driver.java
@@ -0,0 +1,17 @@
+public class Driver
+{
+    
+
+    public static void main(String[] args) {
+	Board b = new Board();
+	GUI x = new GUI(b);
+	x.setVisible(true);
+	/*
+	Player One = new Player(name1.getText());
+	Player Two = new Player(name2.getText());
+	Player Three = new Player(name3.getText());
+	Player Four = new Player(name4.getText());
+	*/
+    }
+
+}
