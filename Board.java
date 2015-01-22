c42da96dfbd2adf3d27f4d9c4b44e51ff3bc062e
Jion Fairchild <jijiglobe@yahoo.com>
2015-01-22 10:05:06 -0500

    SARAH: made sure scoring does not count words within words

--- Board.java
+++ Board.java
@@ -49,8 +49,10 @@ public class Board {
 	int y = ycoor;
 	int WordPoints = 0;
 	int bonus = 1;
+	boolean TileMode = false;
 
 	while(board[x][y].getTileMode() != 0) {
+	    if(board[x][y].getTileMode() == 2) { TileMode = true; }
 	    word = word + board[x][y].getLetter();
 	    WordPoints = WordPoints + board[x][y].getPoints();
 	    bonus = bonus * board[x][y].getWBonus();
@@ -58,7 +60,7 @@ public class Board {
 	}
 	WordPoints = WordPoints * bonus;
 	
-        if(checkWord(word)) {
+        if(checkWord(word) && TileMode) {
 	    return WordPoints;
 	}
 	return 0;
@@ -70,8 +72,10 @@ public class Board {
 	int y = ycoor;
 	int WordPoints = 0;
 	int bonus = 1;
+	boolean TileMode = false;
 
 	while(board[x][y].getTileMode() != 0) {
+	    if(board[x][y].getTileMode() == 2) { TileMode = true; }
 	    word = word + board[x][y].getLetter();
 	    WordPoints = WordPoints + board[x][y].getPoints();
 	    bonus = bonus * board[x][y].getWBonus();
@@ -79,25 +83,39 @@ public class Board {
 	}
 	WordPoints = WordPoints * bonus;
 	
-	if(checkWord(word)) {
+	if(checkWord(word) && TileMode) {
 	    return WordPoints;
 	}
 	return 0;
     }
-
     // --- Finds which words to calculate points for
     public int getPoints() {
         int points = 0;
+	boolean empty = true;
 	
-	for(int x=0; x<board.length; x++) {
-	    for(int y = 0; y<board.length; y++) {
-		if(board[x][y].getTileMode()==2) {
-		    points = points + getPointsH(x, y);
+	for(int x = 0; x < board.length; x++) {
+	    for(int y  = 0; y < board[x].length; y++) {
+		if(empty && board[x][y].getTileMode() != 0) {
 		    points = points + getPointsV(x, y);
-		    board[x][y].setTileMode(3);
+		    empty = false;
+		} else if(board[x][y].getTileMode() == 0) {
+		    empty = true;
 		}
 	    }
 	}
+	
+	empty = true;
+	for(int y = 0; y < board.length; y++) {
+	    for(int x = 0; x < board[y].length; x++) {
+		if(empty && board[x][y].getTileMode() != 0) {
+		    points = points + getPointsH(x, y);
+		    empty = false;
+		} else if(board[x][y].getTileMode() == 0) {
+		    empty = true;
+		}
+	    }
+	}
+	
 	return points;
     }
 
@@ -119,13 +137,9 @@ public class Board {
 	board.assignLetter("L",3,1);
 	board.assignLetter("L",4,1);
 	board.assignLetter("O",5,1);	
-	System.out.println(board.getTile(1,1).getPoints());
-	System.out.println(board.getTile(2,1).getPoints());
-	System.out.println(board.getTile(3,1).getPoints());
-	System.out.println(board.getTile(4,1).getPoints());
-	System.out.println(board.getTile(5,1).getPoints());
-	System.out.println(board.getPointsH(1,1));
+	System.out.println(board.getPoints());
     }
     */
+    
 
 }
